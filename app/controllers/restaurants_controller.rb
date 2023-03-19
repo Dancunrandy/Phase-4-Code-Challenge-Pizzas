7e553c9face418c5c7cd8415f4791e92a11c5052
dancunrandy <dancunrandy@gmail.com>
2023-03-19 23:59:51 +0300

    added CRUD

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,42 @@
 class RestaurantsController < ApplicationController
-end
+    def index
+      restaurants = Restaurant.all
+      render json: restaurants, status: :ok
+    end
+  
+    def show
+      restaurant = Restaurant.find(params[:id])
+      render json: restaurant, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Restaurant not found" }, status: :not_found
+    end
+  
+    def create
+      restaurant = Restaurant.create(restaurant_params)
+      render json: restaurant, status: :created
+    rescue ActionController::ParameterMissing
+      render json: { error: "Restaurant name is required" }, status: :unprocessable_entity
+    end
+  
+    def update
+      restaurant = Restaurant.find(params[:id])
+      restaurant.update(restaurant_params)
+      render json: restaurant, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Restaurant not found" }, status: :not_found
+    end
+  
+    def destroy
+      restaurant = Restaurant.find(params[:id])
+      restaurant.destroy
+      head :no_content
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Restaurant not found"}, status: :not_found
+    end
+    
+    private
+    
+    def restaurant_params
+    params.require(:restaurant).permit(:name)
+    end
+    end
