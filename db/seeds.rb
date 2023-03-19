8d41b84a7f2fabdc053082b8496ad85f9804968c
dancunrandy <dancunrandy@gmail.com>
2023-03-19 23:51:45 +0300

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do
+    restaurant = Restaurant.create(name: Faker::Restaurant.name)
+    pizza = Pizza.create(name: Faker::Food.dish)
+  
+    RestaurantPizza.create(
+      restaurant: restaurant,
+      pizza: pizza,
+      price: rand(1..30)
+    )
+  end
+  
