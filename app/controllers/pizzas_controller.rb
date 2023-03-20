a72ac80ece3ddb047770f1842982e7d6fbca6f0b
dancunrandy <dancunrandy@gmail.com>
2023-03-20 10:04:56 +0300

    added address column

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,48 @@
 class PizzasController < ApplicationController
-end
+    def index
+      pizzas = Pizza.all
+      render json: pizzas
+    end
+  
+    def show
+      pizza = Pizza.find(params[:id])
+      render json: pizza
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Pizza not found" }, status: :not_found
+    end
+  
+    def create
+      pizza = Pizza.new(pizza_params)
+      if pizza.save
+        render json: pizza, status: :created
+      else
+        render json: { error: "Failed to create pizza" }, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      pizza = Pizza.find(params[:id])
+      if pizza.update(pizza_params)
+        render json: pizza
+      else
+        render json: { error: "Failed to update pizza" }, status: :unprocessable_entity
+      end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Pizza not found" }, status: :not_found
+    end
+  
+    def destroy
+      pizza = Pizza.find(params[:id])
+      pizza.destroy
+      head :no_content
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Pizza not found" }, status: :not_found
+    end
+  
+    private
+  
+    def pizza_params
+      params.require(:pizza).permit(:name, :description, :image_url)
+    end
+  end
+  
--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,43 @@
 class RestaurantPizzasController < ApplicationController
-end
+    def index
+      restaurant_pizzas = RestaurantPizza.all
+      render json: restaurant_pizzas
+    end
+  
+    def show
+      restaurant_pizza = RestaurantPizza.find(params[:id])
+      render json: restaurant_pizza
+    end
+  
+    def create
+      restaurant_pizza = RestaurantPizza.new(restaurant_pizza_params)
+  
+      if restaurant_pizza.save
+        render json: restaurant_pizza, status: :created
+      else
+        render json: restaurant_pizza.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      restaurant_pizza = RestaurantPizza.find(params[:id])
+  
+      if restaurant_pizza.update(restaurant_pizza_params)
+        render json: restaurant_pizza
+      else
+        render json: restaurant_pizza.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      restaurant_pizza = RestaurantPizza.find(params[:id])
+      restaurant_pizza.destroy
+    end
+  
+    private
+  
+    def restaurant_pizza_params
+      params.require(:restaurant_pizza).permit(:restaurant_id, :pizza_id, :price)
+    end
+  end
+  
