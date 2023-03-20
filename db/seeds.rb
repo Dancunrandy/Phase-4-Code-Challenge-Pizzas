2fc6e34dbd77001f93e235cf102685988f155edf
dancunrandy <dancunrandy@gmail.com>
2023-03-20 10:13:35 +0300

    seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
-
-
 10.times do
-    restaurant = Restaurant.create(name: Faker::Restaurant.name)
-    pizza = Pizza.create(name: Faker::Food.dish)
-  
-    RestaurantPizza.create(
-      restaurant: restaurant,
-      pizza: pizza,
-      price: rand(1..30)
-    )
-  end
-  
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address
+  )
+  pizza = Pizza.create(
+    name: Faker::Food.dish,
+    ingredients: Faker::Food.ingredient
+  )
+
+  RestaurantPizza.create(
+    restaurant: restaurant,
+    pizza: pizza,
+    price: rand(1..30)
+  )
+end
